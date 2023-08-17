56d2352a271fc04487e237a68a90a13a436b6b90
Burhan Uddin Baig <82936082+burhanuddinbaig@users.noreply.github.com>
2023-08-17 16:46:40 +0500

    seperate files for products

--- model/users.js
+++ model/users.js
@@ -11,32 +11,6 @@ module.exports = class Users {
     return db.execute(`SELECT * FROM  users WHERE userId = ${id}
      `);
   }
-  fetchAllProducts() {
-    return db.execute(`SELECT p.*, u.userId, u.username,ac.productId as auctionProduct,ac.reservePrice,ac.quantity as reserveQuantity,ac.highestBidderId,ac.highestBid,ac.endTime,ac.status as aucStatus FROM products p
-    JOIN users u ON
-    (p.userId = u.userId)
-    LEFT JOIN auction ac ON
-    (p.productId = ac.productId
-      AND
-      ac.status = 1)
-
-     `);
-  }
-  fetchSingleProduct(productId) {
-    return db.execute(`SELECT p.*, u.userId, u.username,ac.productId as auctionProduct,ac.auctionId,ac.reservePrice,ac.quantity as reserveQuantity,ac.highestBidderId,ac.highestBid,ac.endTime,ac.status as aucStatus FROM products p
-    JOIN users u ON
-    (p.userId = u.userId)
-    LEFT JOIN auction ac ON
-    (p.productId = ac.productId
-      AND
-      ac.status = 1
-      )
-    WHERE
-    p.productId= ${productId}
-
-     `);
-  }
-
   logIn(email) {
     return db.execute(`SELECT * FROM  users
     where
@@ -74,69 +48,10 @@ userId = '${userId}'
     return db.execute(`INSERT INTO users SET username = '${username}', email = '${email}', password = '${password}', role = '${role}'
 `);
   }
-
-  addProduct({ title, description, rate, quantity, userId }, image) {
-    return db.execute(`INSERT INTO products SET title = '${title}', description = '${description}',image ='${image}', rate = '${rate}',quantity = '${quantity}', userId = ${userId}
-`);
-  }
-  addBid({ userId, productId, price, auctionId }) {
-    return db.execute(`INSERT INTO bidding SET userId = ${userId}, productId = ${productId}, price = ${price}
-`);
-  }
-  updateAuctionBidding({ userId, price, productId, endTime }) {
-    return db.execute(`UPDATE auction SET highestBidderId = ${userId}, highestBid = '${price}',endTime = '${endTime}'  WHERE
-    productId = ${productId}
-`);
-  }
-  listOnAuction({ userId, productId, reservePrice, quantity }) {
-    return db.execute(`INSERT INTO auction SET userId = ${userId}, productId = ${productId}, reservePrice = ${reservePrice},quantity = '${quantity}'
-`);
-  }
-  updateListing(productId) {
-    return db.execute(`UPDATE products SET isListed = 1
-WHERE
-productId = ${productId}
-`);
-  }
   deleteUser(userId) {
     return db.execute(`DELETE FROM users
 WHERE
 userId  = ${userId}
-`);
-  }
-  deleteProduct(productId) {
-    return db.execute(`DELETE FROM products
-WHERE
-productId   = ${productId}
-`);
-  }
-  searchProduct(title) {
-    return db.execute(`SELECT * FROM  products
-    WHERE
-    title LIKE '%${title}%' `);
-  }
-  createdPprducts(userId) {
-    return db.execute(`SELECT p.*, u.userId, u.username,ac.productId as auctionProduct,ac.reservePrice,ac.quantity as reserveQuantity,ac.highestBidderId,ac.highestBid,ac.endTime,ac.status as aucStatus FROM products p
-    JOIN users u ON
-    (p.userId = u.userId)
-    LEFT JOIN auction ac ON
-    (p.productId = ac.productId
-      AND ac.status = 1)
-    WHERE
-    u.userId = ${userId}
-
-     `);
-  }
-  updateProductQuantity(productId, quantity) {
-    return db.execute(`UPDATE products SET quantity = '${quantity}' , isListed = 0
-WHERE
-productId = ${productId}
-`);
-  }
-  settleAuction(auctionId) {
-    return db.execute(`UPDATE auction SET status = 0
-     WHERE
-auctionId  = ${auctionId}
 `);
   }
 };
